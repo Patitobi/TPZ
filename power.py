5b4c2f16d6c2675d67908a9ec196db9aaf5495ac
Patitobi <100159085+Patitobi@users.noreply.github.com>
2022-02-28 00:26:19 +0100

    Add files via upload

--- power.py
+++ power.py
@@ -1,23 +1,41 @@
 from time import sleep
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
+import pygame
+from pygame.locals import * 
 
-GPIO.setup(14, GPIO.OUT)
+pygame.init()
+screen=pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Test')
+pygame.mouse.set_visible(0)
+
 while True:
+    print('go')
+    
+    for event in pygame.event.get():
+        if (event.type==KEYUP) or (event.type==KEYDOWN):
+            print('entlich')
+            sleep(0.1)
+
+while True:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(14,GPIO.OUT)
+    GPIO.setup(15, GPIO.OUT)
     print('Ein') 
     GPIO.output(14,GPIO.HIGH)
+    GPIO.output(15,GPIO.HIGH)
 
     sleep(5)
     print('Aus')
-
-    GPIO.output(14, GPIO.LOW)
+    GPIO.cleanup()
     
     print('weiter = 1 stop = 0')
     eingabe = int(input())
     if eingabe == 0 :
+        GPIO.cleanup()
+        print('beenden Teil 1')
         break
-        print('beenden')
     
-GPIO.cleanup()
+    
+    
     
 
