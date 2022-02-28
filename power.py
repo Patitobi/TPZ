6d0d6737d3435a2e49a5038f8048ed32eff2c8c3
Patitobi <100159085+Patitobi@users.noreply.github.com>
2022-02-28 13:59:43 +0100

    Add files via upload

--- power.py
+++ power.py
@@ -9,31 +9,44 @@ pygame.display.set_caption('Test')
 pygame.mouse.set_visible(0)
 
 while True:
-    print('go')
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(15, GPIO.OUT)
+    GPIO.setup(16, GPIO.OUT)
     
     for event in pygame.event.get():
-        if (event.type==KEYUP) or (event.type==KEYDOWN):
-            print('entlich')
-            sleep(0.1)
-
-while True:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(14,GPIO.OUT)
-    GPIO.setup(15, GPIO.OUT)
-    print('Ein') 
-    GPIO.output(14,GPIO.HIGH)
-    GPIO.output(15,GPIO.HIGH)
-
-    sleep(5)
-    print('Aus')
-    GPIO.cleanup()
-    
-    print('weiter = 1 stop = 0')
-    eingabe = int(input())
-    if eingabe == 0 :
-        GPIO.cleanup()
-        print('beenden Teil 1')
-        break
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            main = False
+            break
+            
+        if (event.type==pygame.KEYDOWN):
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                print('LEFT DOWN')
+                sleep(0.1)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                print('RIGHT DOWN')
+                sleep(0.1)
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                print('UP DOWN')
+                sleep(0.1)
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                print('DOWN DOWN')
+                sleep(0.1)
+            
+        if (event.type==pygame.KEYUP):
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                print('LEFT UP')
+                sleep(0.1)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                print('RIGHT UP')
+                sleep(0.1)
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                print('UP UP')
+                
+                sleep(0.1)
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                print('DOWN UP')
+                sleep(0.1)
     
     
     
